05a0a70ffe388557634eec8712060ff30125d456
adrianna hatcher <amhatcher@bsu.edu>
2024-01-18 15:02:31 -0500

    initial commit

---
+++ FactLibrary/FactorialCalculator.cs
@@ -0,0 +1,26 @@
+﻿namespace FactLibrary;
+
+public class FactorialCalculator
+{
+    public long Calculate(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException("Factorial is not defined for negative numbers.");
+        }
+
+        if (number == 0 || number == 1)
+        {
+            return 1;
+        }
+
+        long result = 1;
+        for (int i = 2; i <= number; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+}
+
---
+++ FactTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace FactTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
